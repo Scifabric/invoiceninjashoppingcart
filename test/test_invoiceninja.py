8306318dcb35cdb0701e751175ebb1c02f77e2c3
Daniel Lombraña González <teleyinex@gmail.com>
2016-07-18 15:33:35 +0200

    Fixes.

--- test/test_invoiceninja.py
+++ test/test_invoiceninja.py
@@ -10,7 +10,11 @@ class TestInvoiceNinja(object):
     @patch('invoiceninja.invoiceNinja.get_static_data', return_value=data.static)
     def test_invoice_ninja_setup(self, mock_data):
         """Test invoice ninja setup works."""
-        iv = invoiceNinja(token='token')
+        iv = invoiceNinja(token='token', url='http://myurl.com')
 
         err_msg = "invoice ninja should have been created with static data."
         assert iv.static == data.static, err_msg
+        assert iv.token == 'token', err_msg
+        assert iv.url == 'http://myurl.com', err_msg
+        assert 'X-Ninja-Token' in iv.headers.keys(), err_msg
+        assert iv.headers['X-Ninja-Token'] == 'token', err_msg
